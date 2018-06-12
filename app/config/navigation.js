fba6a95e125609357b089f3e7a9fe247b1556a56
Marcus Kopp <marcus.kopp86@gmail.com>
2018-06-12 11:19:51 +0200

    Implemented badge from scratch

--- app/config/navigation.js
+++ app/config/navigation.js
@@ -1,30 +1,74 @@
 import React from 'react';
-import { createBottomTabNavigator } from 'react-navigation';
+import { StyleSheet, Text, View } from 'react-native';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
 
 import Home from '../views/Home';
 import First from '../views/First';
 import Second from '../views/Second';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export const Tab = createBottomTabNavigator({
-  Home: {
-    screen: Home
-  },
-  First: {
-    screen: First
-  },
-  Second: {
-    screen: Second
-  }
-}, {
-  tabBarPosition: 'bottom',
-  swipeEnabled: true,
-  tabBarOptions: {
-    activeTintColor: '#f2f2f2',
-    activeBackgroundColor: '#2EC4B6',
-    inactiveTintColor: '#666',
-    labelStyle: {
-      fontSize: 22,
-      padding: 12
+/**
+ * implementation done according to example by nguyenvanphuc from
+ * https://github.com/oblador/react-native-vector-icons/issues/292
+ */
+
+export const Tab = TabNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    First: {
+      screen: First
+    },
+    Second: {
+      screen: Second
     }
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        let badgeText; // parameter to make number
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          badgeText = '2';
+          return (
+            <View style={styles.badgeIconView}>
+              <Text style={styles.badge}> {badgeText} </Text>
+              <Ionicons name={iconName} size={25} color={tintColor} />
+            </View>
+          );
+        } else {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+          return <Ionicons name={iconName} size={25} color={tintColor} />;
+        }
+      }
+    }),
+    tabBarComponent: TabBarBottom,
+    tabBarPosition: 'bottom',
+    swipeEnabled: true,
+    tabBarOptions: {
+      activeTintColor: '#f0f',
+      inactiveTintColor: '#666'
+    }
+  }
+);
+
+const styles = StyleSheet.create({
+  badgeIconView: {
+    padding: 5,
+    position: 'relative'
+  },
+  badge: {
+    backgroundColor: '#0ff',
+    borderRadius: 1,
+    color: '#fff',
+    fontSize: 8,
+    position: 'absolute',
+    padding: 1,
+    right: 1,
+    top: 1,
+    zIndex: 10
   }
 });
